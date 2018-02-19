3be9e0ea460842767c5b7e7f6a32c7fddd918cd9
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-02-19 17:06:34 +0000

    Fix whitespace

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'twitter'
 before do
   config = {
       :consumer_key => 'vqJ9GQDOns00WAQx7oDoBJqFX',
-      :consumer_secret => '	HmlsBdLGWcZXcFCiidsq74AlLr2XWJnKaO09QPxMWN24ZdpxjK',
-      :access_token => '	965615031185485824-xCVnWm6q628bwhbLcFjhAsGCpPojDKT',
-      :access_token_secret => '	ilEkrfVAahr8odgafGkodNkRl045MbbSgeoicL4x80EB8'
+      :consumer_secret => 'HmlsBdLGWcZXcFCiidsq74AlLr2XWJnKaO09QPxMWN24ZdpxjK',
+      :access_token => '965615031185485824-xCVnWm6q628bwhbLcFjhAsGCpPojDKT',
+      :access_token_secret => 'ilEkrfVAahr8odgafGkodNkRl045MbbSgeoicL4x80EB8'
   }
   @client = Twitter::REST::Client.new(config)
 end
