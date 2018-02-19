766997201dbbbfb4c75070f3f9fd316439a28934
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-02-19 17:01:15 +0000

    Updated twitter keys

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'twitter'
 
 before do
   config = {
-      :consumer_key => '...',
-      :consumer_secret => '...',
-      :access_token => '...',
-      :access_token_secret => '...'
+      :consumer_key => 'vqJ9GQDOns00WAQx7oDoBJqFX',
+      :consumer_secret => '	HmlsBdLGWcZXcFCiidsq74AlLr2XWJnKaO09QPxMWN24ZdpxjK',
+      :access_token => '	965615031185485824-xCVnWm6q628bwhbLcFjhAsGCpPojDKT',
+      :access_token_secret => '	ilEkrfVAahr8odgafGkodNkRl045MbbSgeoicL4x80EB8'
   }
   @client = Twitter::REST::Client.new(config)
 end
