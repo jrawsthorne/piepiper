7b8a0e87603dfd27720f246a42296a51be976c7b
lucasmaybury <lucasmaybury@gmail.com>
2018-03-06 16:36:48 +0000

    new_user function started
    Waiting on database to be completed to fully implement, as dietary requirements not in place yet.

--- app.rb
+++ app.rb
@@ -51,3 +51,46 @@ get '/auth/twitter/callback' do
     redirect '/signup'
   end
 end
+
+=begin
+post '/signup' do
+  new_user(
+    params[:username],
+    params[:firstname],
+    params[:lastname],
+    params[:password],
+    params[:twitter_handle],
+    params[:dietaryrequirements],
+    params[:house_number],
+    params[:address_line_1],
+    params[:address_line_2],
+    params[:city_region],
+    params[:postcode]
+          )
+end
+=end
+
+def new_user(username, firstname, lastname, password,
+            twitter_id, special_conditions,
+            house_number, address_line_1, address_line_2,
+            city_region, postcode)
+  user = User.new do |u|
+    u.username = username
+    u.firstname = firstname
+    u.lastname = lastname
+    u.password = "password"
+    u.twitter_id = twitter_id.to_s
+    special_conditions.each do |condition|
+      
+    end
+    u.house_number = house_number
+    u.address_line_1 = address_line_1
+    u.address_line_2 = address_line_2
+    u.city_region = city_region
+    u.postcode = postcode
+    u.account_type_id = 1
+  end
+  user.save
+  session[:user_id] = user.id
+  puts session[:user_id]
+end
