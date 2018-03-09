d72f69becfc5086eefe0688ffc514ca76f8026ab
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-03-09 18:53:38 +0000

    A few methods to check user account type

--- helpers/user.rb
+++ helpers/user.rb
@@ -16,3 +16,20 @@ end
 def get_account_type_from_session
   return User.where(id: session[:user_id]).pluck(:account_type_id).join.to_i if session[:user_id]
 end
+
+def admin!
+  check_account_type(3)
+end
+  
+def order_handler!
+  check_account_type(2);
+end
+
+def check_account_type(account_type_id)
+  authenticate!
+  unless(get_account_type_from_session >= account_type_id)
+    flash[:error] = "Unauthorised access, you've been signed out"
+    session[:user_id] = nil
+    redirect '/login'
+  end
+end
