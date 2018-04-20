0ea1235cb4e6fb77089e3f9feac8f59027875553
Seth Faulkner <sfaulkner1@sheffield.ac.uk>
2018-04-20 13:49:54 +0100

    Can now add Campaigns to database

--- routes/admin/campaign.rb
+++ routes/admin/campaign.rb
@@ -21,7 +21,15 @@ end
 post '/campaigns' do
 	if(authenticated?)
 		 tweet=$client.update(params[:message])
-		 puts tweet.id
+		 reward_type = params[:reward]
+
+		 campaign = Campaign.new do |u|
+		 	u.campaign_type_id = reward_type
+		 	u.tweet_id = tweet.id
+		 	#u.winner_number = winner_number
+		 end
+		 campaign.save
+
 	else
 		redirect('/')
 	end
