0595e17e534d389f6ede5cf97975633fb2679ab4
Seth Faulkner <sfaulkner1@sheffield.ac.uk>
2018-04-20 14:43:18 +0100

    adds winner number

--- routes/admin/campaign.rb
+++ routes/admin/campaign.rb
@@ -22,11 +22,11 @@ post '/campaigns' do
 	if(authenticated?)
 		 tweet=$client.update(params[:message])
 		 reward_type = params[:reward]
-
+		 winner_number = params[:winner_number]
 		 campaign = Campaign.new do |u|
 		 	u.campaign_type_id = reward_type
 		 	u.tweet_id = tweet.id
-		 	#u.winner_number = winner_number
+		 	u.winner_number = winner_number
 		 end
 		 campaign.save
 
