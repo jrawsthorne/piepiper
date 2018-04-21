1429083f01b4dc0810983f8c608ddd69b75631ec
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-04-21 16:18:37 +0100

    Get user latitude and longitude and send to google Call set location and then submit the form

---
+++ public/scripts/login.js
@@ -0,0 +1,33 @@
+$("form[id='login']").submit(function(e){
+  e.preventDefault();
+  const form = this
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getCity, submit, {enableHighAccuracy: true, timeout: 5000, maximumAge: 0});
+  } else {
+    form.submit()
+  }
+  // Get the latitude and the longitude;
+  function getCity(position) {
+    const latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+    new google.maps.Geocoder().geocode({'latLng': latlng}, (results, status) => {
+      if (status == google.maps.GeocoderStatus.OK) {
+        const city = results.find(result => result.types[0] === "locality").address_components[0].long_name
+          fetch('/set-location', {
+            credentials: "same-origin",
+            method: 'POST',
+            body: JSON.stringify({city}),
+            headers: new Headers({
+              'Content-Type': 'application/json'
+            })
+          })
+          .then(() => form.submit())
+      } else {
+        form.submit();
+      }
+    });
+  }
+  
+  function submit() {
+    form.submit()
+  }
+});
