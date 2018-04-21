47b9170207e407ceb16954a539c147bf275cf39a
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2018-04-21 16:15:50 +0100

    If location not set, set it to first in table Don't send email on login (was for testing) Set location to nil on logout Set session to location id if it exists in db

--- routes/user.rb
+++ routes/user.rb
@@ -17,28 +17,32 @@ class PiePiper < Sinatra::Base
       flash[:error] = "You're already signed in"
     end
     @title = "Login"
+    @js = ['/scripts/login.js']
     erb :'/user/login'
   end
 
   post '/login' do
+    if(session[:location_id] === nil)
+      session[:location_id] = Location.first.id
+    end
     user = User.find_by_username(params[:username])
     if !user.nil? && user.password == params[:password]
       session[:user_id] = user.id
-      Pony.mail({
-        :to => 'jake@jakerawsthorne.co.uk',
-        :from => 'no-reply@pie-piper.me',
-        :subject => 'Logged in',
-        :body => 'Thanks for loggin in '+user.fullname,
-        :via => :smtp,
-        :via_options => {
-          :address              => 'smtp.zoho.eu',
-          :port                 => '587',
-          :user_name            => 'no-reply@pie-piper.me',
-          :password             => 'sb3I5S7Cj9*5',
-          :authentication       => :plain,
-          :enable_starttls_auto => true,
-        }
-      })
+      #Pony.mail({
+      #  :to => 'jake@jakerawsthorne.co.uk',
+      #  :from => 'no-reply@pie-piper.me',
+      #  :subject => 'Logged in',
+      #  :body => 'Thanks for loggin in '+user.fullname,
+      #  :via => :smtp,
+      #  :via_options => {
+      #    :address              => 'smtp.zoho.eu',
+      #    :port                 => '587',
+      #    :user_name            => 'no-reply@pie-piper.me',
+      #    :password             => 'sb3I5S7Cj9*5',
+      #    :authentication       => :plain,
+      #    :enable_starttls_auto => true,
+      #  }
+      #})
       redirect '/'
     else
       flash[:error] = "Incorrect credentials"
@@ -63,7 +67,23 @@ class PiePiper < Sinatra::Base
 
   get '/logout' do
     session[:user_id] = nil
+    session[:location_id] = nil
     redirect '/'
+  end
+  
+  post '/set-location' do
+    payload = params
+    payload = JSON.parse(request.body.read).symbolize_keys unless params[:path]
+    city = payload[:city]
+    if(Location.exists?(name: city))
+      location = Location.find_by(name: city)
+      session[:location_id] = location.id
+      status 200
+    else
+      location = Location.first
+      session[:location_id] = 1
+      status 200
+    end
   end
 
   not_found do
