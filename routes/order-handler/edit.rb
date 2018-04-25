db12f26dcbf9df2860fa49041aab2e2946d1577e
Jake Rawsthorne <jrawsthorne1@sheffield.ac.uk>
2018-04-25 13:46:39 +0000

    Change order state and sends tweet on ready

--- routes/order-handler/edit.rb
+++ routes/order-handler/edit.rb
@@ -2,7 +2,18 @@ class PiePiper < Sinatra::Base
   post '/edit-order/:order_id' do
     @error = check_input(params[:item],params[:quantity])
     if(@error == false)
-      Order.find(params[:order_id]).edit_order(params[:item], params[:quantity], params[:order_state])
+      order = Order.find(params[:order_id])
+      order.edit_order(params[:item], params[:quantity])
+      order.order_state_id = params[:order_state]
+      order.save
+      if(params[:order_state] == "2")
+          user = order.get_tweet.user.screen_name
+          tweet = order.get_tweet.id
+          name = User.find(order.user_id).fullname
+          text = "your order is ready to collect"
+          puts "@"+user+" "+name + ", " + text
+          $client.update("@"+user+" "+name + ", " + text, in_reply_to_status_id: tweet)
+      end
       redirect '/orders'
     else
       @title = "Edit order"
