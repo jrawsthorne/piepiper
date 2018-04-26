2c512117aafa7270647ca8e2042295aa9705f4cc
lucasmaybury <lucasmaybury@gmail.com>
2018-04-26 17:11:00 +0100

    User Account Type Change Warning Message
    Popup to ask user if they want to change user to ADmin/Handler. Clicking Cancel resets it

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,8 +1,21 @@
 $("select").on('change', function() {
-  if($(this).val() == 3){
-    alert("Warning: "+$(this).data('user')+" will be granted administrator priviliges with this change.")
+  if($(this).val() == 3) {
+    if(!confirm("Warning: User with ID number: "+$(this).data('user')+" will be granted administrator priviliges with this change. Are you sure you want to make this change?")) {
+      resetForm("select"+$(this).data('user'), $(this).data('default'))
+    }
   }
   else if ($(this).val() == 2) {
-    alert("Warning: "+$(this).data('user')+" will be granted order-handler priviliges with this change.")
+    if(!confirm("Warning: User with ID number: "+$(this).data('user')+" will be granted order-handler priviliges with this change. Are you sure you want to make this change?")) {
+      resetForm("select"+$(this).data('user'), $(this).data('default'))
+    }
   }
 })
+
+function resetForm(id, defaultVal) {
+  for(var index = 0; index < (document.getElementById(id).options.length); index++ ){
+    if( document.getElementById(id).options[index].value == defaultVal ){
+      document.getElementById(id).selectedIndex = index;
+      break;
+    }
+  }
+}
