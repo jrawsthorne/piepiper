34e04cf7ca1b335b558e23c87bb27fd5a0cda2b9
Collin Coutinho <ccoutinho1@sheffield.ac.uk>
2018-04-27 22:03:45 +0100

    Redundant Code

--- helpers/item.rb
+++ helpers/item.rb
@@ -5,13 +5,7 @@
 
   def new_item(
   	name, price, types, special_condition_id, locations)
-<<<<<<< HEAD
     
-=======
-
-  
-
->>>>>>> c773951b9240cb0f8dc2d43090281f8c150d1edb
   	item = Item.new do |u|
   		u.name = name
   		u.price = price
